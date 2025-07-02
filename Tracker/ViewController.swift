da2c1b5be0dadeee5c77094efac3524b8f74e95f
R Kolos <rkolos@MacBook-Pro-R-2.local>
2025-07-02 06:52:16 -0700

    Main, started project

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by R Kolos on 02.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
